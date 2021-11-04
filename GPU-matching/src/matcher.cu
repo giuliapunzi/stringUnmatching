eb1fce8d507d0f642ea17271d07066a4fa69c7d1
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-11-04 22:06:10 +0100

    Fix kernels

--- GPU-matching/src/matcher.cu
+++ GPU-matching/src/matcher.cu
@@ -1,6 +1,9 @@
 #include "matcher.hpp"
+#include "io.hpp"
 #include "cuda_helper.cuh"
 #include <cuda_runtime.h>
+#include <algorithm>
+#include <climits>
 #include <cmath>
 
 #ifndef BLOCK_DIM
@@ -17,6 +20,7 @@ void expand_kernel(byte_t* matrix, length_t length) {
         auto current = matrix[idx];
         auto next = idx == length - 1? (byte_t) 0 : matrix[idx + 1];
 
+        #pragma unroll
         for (auto i = 1; i < io::Q; ++i) { 
             matrix[idx + i*length] = (byte_t) ((current << 2*i) | (next >> 2*(io::Q - i)));
         }
@@ -44,37 +48,53 @@ byte_t hamming_distance(chunk_t x, chunk_t y) {
     return (byte_t) (NUM_NUCLEOTIDES - __popcll(diff));
 }
 
+__device__ 
+void min_reduce_kernel(byte_t* data, length_t length, unsigned int num_blocks) {
+    auto idx = threadIdx.x + blockIdx.x * blockDim.x;
+    
+    auto stride = num_blocks/2;
+    auto next_idx = idx + stride*blockDim.x;
+    num_blocks = max(stride, num_blocks - stride);
+
+    while (stride && blockIdx.x < num_blocks && next_idx < length) {
+        data[idx] = min(data[idx], data[next_idx]);
+    
+        __syncthreads();
+
+        stride = num_blocks/2;
+        next_idx = idx + stride*blockDim.x;
+        num_blocks = max(stride, num_blocks - stride);
+    }    
+}
+
 __global__
 void min_hamming_distance_kernel(chunk_t sample, const byte_t* bytes, byte_t* result,
                                  length_t length, char excess = 0) {
     auto idx = threadIdx.x + blockIdx.x * blockDim.x;
-    auto limit = length - CHUNK_SIZE + 1 - !!excess;
+    
+    if (idx < length) {
+        result[idx] = UCHAR_MAX;
+    }
+
+    __syncthreads();
+
+    auto limit = length - CHUNK_SIZE + 1;
 
     for (auto i = 0; i < io::Q; ++i) {
-        if (idx*io::Q + i < limit*io::Q - excess) {
-            auto chunk = (chunk_t) *(bytes + idx + i*length);
+        if (idx*io::Q + excess + i < limit) {
+            auto chunk = *((chunk_t*) (bytes + idx + i*length));
             auto dist = hamming_distance(sample, chunk);
 
-            if (i == 0 || dist < result[idx]) {
-                result[idx] = dist;
-            }
+            result[idx] = min(dist, result[idx]);
         }
     }
 
     __syncthreads();
 
-    auto stride = blockDim.x * gridDim.x;
-
-    while (stride >>= 1) {
-        if (idx < stride && idx + stride < limit) {
-            result[idx] = min(result[idx], result[idx + stride]);
-        }
-
-        __syncthreads();
-    }
+    min_reduce_kernel(result, limit, gridDim.x);
 }
 
-char Matcher::min_hamming_distance(chunk_t sample) {
+byte_t Matcher::min_hamming_distance(chunk_t sample) {
     byte_t* distances;
 
     CUDA_CHECK(cudaMalloc((void**) &distances, length))
@@ -84,12 +104,13 @@ char Matcher::min_hamming_distance(chunk_t sample) {
 
     min_hamming_distance_kernel<<<grid_dim, block_dim>>>(sample, d_bytes, distances, length, excess);
 
-    char result = -1;
+    byte_t result[BLOCK_DIM];
+    auto limit = std::min<length_t>(BLOCK_DIM, length - CHUNK_SIZE + 1);
 
-    CUDA_CHECK(cudaMemcpy((byte_t*) &result, distances, 1, cudaMemcpyDeviceToHost))
+    CUDA_CHECK(cudaMemcpy(result, distances, limit*sizeof(byte_t), cudaMemcpyDeviceToHost))
     CUDA_CHECK(cudaFree(distances))
 
-    return result;
+    return *std::min_element(result, result + limit);
 }
 
 Matcher::~Matcher() {
